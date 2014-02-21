fb99ac0311f1f74eae69a70b17c6721aa7d714b0
danielspector <danielyspector@gmail.com>
2014-02-21 13:42:01 -0500

    set up file structure

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'activerecord'
+gem 'json'
+gem 'rest-client'
+
+group :development do
+  gem 'pry'
+end
+
+group :test do
+  gem 'rspec'
+end
---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/class/artist'
+require_relative '../lib/class/genre'
+require_relative '../lib/class/song'
+require_relative '../lib/class/library_parser'
+
+require 'erb'
+
+require 'bundler/setup'
+Bundler.require
+
